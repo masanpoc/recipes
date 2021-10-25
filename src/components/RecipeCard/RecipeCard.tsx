c556ec36a41829d4347e8d920286c269911470ee
Mario Sancho <marioscp1998@outlook.es>
2021-10-25 12:02:13 +0200

    add links to image, title (ux)

--- src/components/RecipeCard/RecipeCard.tsx
+++ src/components/RecipeCard/RecipeCard.tsx
@@ -61,7 +61,7 @@ const StyledTimeDiv = styled.div`
 const StyledTitle = styled('h3')<{length: number}>`
   /* font-size: 2em; */
   ${(props)=>
-    props.length > 59 ? `
+    props.length > 30 ? `
       font-size: 1.7em
     ` : `
       font-size: 2em
@@ -84,9 +84,13 @@ const RecipeCard = ({ image, title, time, source, id }: Props): JSX.Element => {
 
   return (
      <StyledCard>
-      <StyledImg src={image}></StyledImg>
+      <a href={source} rel="noreferrer" target="_blank">
+        <StyledImg src={image}></StyledImg>
+      </a>
       <Wrapper>
-        <StyledTitle length={title.length}>{title}</StyledTitle>
+        <a href={source} rel="noreferrer" target="_blank">
+          <StyledTitle length={title.length}>{title}</StyledTitle>
+        </a>
         <StyledTimeDiv>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm5.848 12.459c.202.038.202.333.001.372-1.907.361-6.045 1.111-6.547 1.111-.719 0-1.301-.582-1.301-1.301 0-.512.77-5.447 1.125-7.445.034-.192.312-.181.343.014l.985 6.238 5.394 1.011z" />
